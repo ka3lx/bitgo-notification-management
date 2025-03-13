ad2fc1e3b8b421bb7a0724d3bd21f7578adec591
Raj Shekhar <channiecx954@gmail.com>
2025-03-13 13:36:34 +0530

    Use routes

---
+++ routes/notifications.js
@@ -0,0 +1,78 @@
+import express from "express";
+import { notifications } from "../store.js";
+
+const router = express.Router({ mergeParams: true });
+
+router.post("/create", async (req, res) => {
+  try {
+    const properties = req.query;
+    const id = crypto.randomUUID();
+    const notification = { id, ...properties };
+    notifications.push(notification);
+    return res.status(200).send(notification);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+router.get("/send", (req, res) => {
+  try {
+    const { emails } = req.body;
+    console.log(emails);
+    // send notifications to the emails mentioned
+    return res.status(200).json({ message: "Success" });
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+router.get("/list", (req, res) => {
+  try {
+    const { status } = req.query;
+    if (status) {
+      if (NOTIFICATION_STATES.includes(status)) {
+        const notificationsList = notifications.filter(
+          (n) => n.status === status
+        );
+        return res.status(200).json({ notifications: notificationsList });
+      } else {
+        return res.status(400);
+      }
+    } else {
+      return res.status(402).json({ notifications });
+    }
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+router.put("/:id", (req, res) => {
+  try {
+    const { id } = req.params;
+    const index = notifications.findIndex((n) => n.id === id);
+    if (index !== -1) {
+      const { id } = notifications[index];
+      const updatedNotification = { id, ...req.body };
+      notifications[index] = updatedNotification;
+      return res.status(200).send(notifications[index]);
+    } else {
+      return res.status(204);
+    }
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+router.delete("/:id", (req, res) => {
+  try {
+    const { id } = req.params;
+    const index = notifications.findIndex((n) => n.id === id);
+    const deletedNotification = notifications[index];
+    notifications.splice(index, 1);
+    return res.status(200).send(deletedNotification);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+export default router;
